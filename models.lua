050ed250190a88ce0d7d01e6cd406c3f3095657b
Rostik22803 <tuzukrostik888@gmail.com>
2026-08-12 03:05:03 -0700

    Update asset IDs for donkey, pig, and demon

--- models.lua
+++ models.lua
@@ -1,7 +1,7 @@
 return {
-    ["donkey"]    = "rbxassetid://5633413387",
+    ["donkey"]    = "rbxassetid://15258571412",
     ["cute girl"] = "rbxassetid://120285863159417",
-    ["pig"]       = "rbxassetid://3907559467",
+    ["pig"]       = "rbxassetid://12928490816",
     ["angel"]     = "rbxassetid://12318430407",
-    ["demon"]     = "rbxassetid://1988558105"
+    ["demon"]     = "rbxassetid://116615563871570"
 }
